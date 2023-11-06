279cee9f731cd4598479d360cea580ec270d0a6b
guilhermetwelve0 <guilherme.cabral999@hotmail.com>
2023-11-06 19:10:37 -0300

    Algumas alterações

--- resources/views/quartos/edit.blade.php
+++ resources/views/quartos/edit.blade.php
@@ -5,27 +5,27 @@
 @section('contents')
     <h1 class="mb-0">Edicao de Quarto</h1>
     <hr />
-    <form action="{{ route('quarto.update', $quarto->id) }}" method="POST">
+    <form action="{{ route('quartos.update', $quartos->id) }}" method="POST">
         @csrf
         @method('PUT')
         <div class="row">
             <div class="col mb-3">
                 <label class="form-label">Nome</label>
-                <input type="text" name="nome" class="form-control" placeholder="Nome do Quarto" value="{{ $quarto->nome }}" >
+                <input type="text" name="nome" class="form-control" placeholder="Nome do Quarto" value="{{ $quartos->nome }}" >
             </div>
             <div class="col mb-3">
                 <label class="form-label">Preco</label>
-                <input type="text" name="preco" class="form-control" placeholder="Preco do Quarto" value="{{ $quarto->preco }}" >
+                <input type="text" name="preco" class="form-control" placeholder="Preco do Quarto" value="{{ $quartos->preco }}" >
             </div>
         </div>
         <div class="row">
             <div class="col mb-3">
                 <label class="form-label">Andar</label>
-                <input type="text" name="andar" class="form-control" placeholder="Andar do quarto" value="{{ $quarto->andar }}" >
+                <input type="text" name="andar" class="form-control" placeholder="Andar do quarto" value="{{ $quartos->andar }}" >
             </div>
             <div class="col mb-3">
                 <label class="form-label">Numero</label>
-                <input type="text" name="numero" class="form-control" placeholder="Numero do Quarto" value="{{ $quarto->numero }}" >
+                <input type="text" name="numero" class="form-control" placeholder="Numero do Quarto" value="{{ $quartos->numero }}" >
             </div>
         </div>
         <div class="row">
